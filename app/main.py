61a19b91d8b519bb200c43a31a293c1a12dba977
Sreeraj <sreeraj@disroot.org>
2020-05-20 09:51:27 +0530

    pc desktop environment crashed , commit to save state

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import Depends, FastAPI, Header, HTTPException
 from sqlalchemy.orm import Session
-
+from fastapi.middleware.cors import CORSMiddleware
 
 from .schemas import users
 
@@ -9,12 +9,28 @@ from .models import movies
 from .models import sync_all
 from .routers import users,movies
 from .connections import cassandra_db
+import global_config
 
 cassandra_db.setup_cassandra_connection()
 
 sync_all.sync_models()
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost:3000",
+   
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(users.router)
 app.include_router(movies.router)
